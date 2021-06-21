0246f254cdc9dd204f01459c7d164d60c6cd108b
Robert <constantinescu.robert94@gmail.com>
2021-06-21 14:12:23 +0300

    added the GreetingClient and the Greet api definition

---
+++ src/main/java/com/java/grpc/hello/client/GreetingClient.java
@@ -0,0 +1,32 @@
+package com.java.grpc.hello.client;
+
+import com.proto.test.TestMessage;
+import com.proto.test.TestServiceGrpc;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
+public class GreetingClient {
+
+    public static void main(String[] args) {
+        System.out.println("This is the gRPC client");
+
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50051)
+                .usePlaintext() //this will disable SSL. Only use in development
+                .build();
+
+        System.out.println("Creating Stub");
+        //creating a SYNCHRONOUS client
+        TestServiceGrpc.TestServiceBlockingStub syncClient = TestServiceGrpc.newBlockingStub(channel);
+
+        //creating an ASYNCHRONOUS client
+        //TestServiceGrpc.TestServiceFutureStub asyncClient = TestServiceGrpc.newFutureStub(channel);
+
+        // do some stuff
+
+        System.out.println("Shutting down channel");
+        channel.shutdown();
+
+
+    }
+
+}
