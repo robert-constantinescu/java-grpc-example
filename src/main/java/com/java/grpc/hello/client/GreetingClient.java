108384c57fb9efcdb9a3fd331c6f7c30e7d8e608
Robert <constantinescu.robert94@gmail.com>
2021-06-22 15:14:13 +0300

    created the code for the GreetingClient

--- src/main/java/com/java/grpc/hello/client/GreetingClient.java
+++ src/main/java/com/java/grpc/hello/client/GreetingClient.java
@@ -1,6 +1,10 @@
 package com.java.grpc.hello.client;
 
 import com.proto.test.TestServiceGrpc;
+import com.proto.unary.GreetRequest;
+import com.proto.unary.GreetResponse;
+import com.proto.unary.GreetServiceGrpc;
+import com.proto.unary.Greeting;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 
@@ -15,12 +19,30 @@ public class GreetingClient {
 
         System.out.println("Creating Stub");
         //creating a SYNCHRONOUS client
-        TestServiceGrpc.TestServiceBlockingStub syncClient = TestServiceGrpc.newBlockingStub(channel);
+//        TestServiceGrpc.TestServiceBlockingStub syncClient = TestServiceGrpc.newBlockingStub(channel);
+
+        //created a greet service client(synchronous)
+        GreetServiceGrpc.GreetServiceBlockingStub greetClient = GreetServiceGrpc.newBlockingStub(channel);
+
+        //created a protocol buffer greeting message
+        Greeting greeting = Greeting.newBuilder()
+                .setFirstName("Richie")
+                .setFirstName("Ares")
+                .build();
+        //created a protocol buffer GreetRequest
+        GreetRequest greetRequest = GreetRequest.newBuilder()
+                .setGreeting(greeting)
+                .build();
+
+        // call the RPC and get back a GreetResponse (
+        GreetResponse response = greetClient.greet(greetRequest);
+
+        // do some stuff
+        System.out.println(response);
 
         //creating an ASYNCHRONOUS client
         //TestServiceGrpc.TestServiceFutureStub asyncClient = TestServiceGrpc.newFutureStub(channel);
 
-        // do some stuff
 
         System.out.println("Shutting down channel");
         channel.shutdown();
